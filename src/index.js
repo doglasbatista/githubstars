0560de6bb5179eb557c868619ba6a5d3fd3aeb87
doglasbatista <doglasbatista@gmail.com>
2019-12-11 15:26:33 -0300

    Added and config apollo client

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 
 import Routes from './Routes';
 
 import './reset.css';
 
+const apolloClient = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+});
+
 ReactDOM.render(
   <Router>
-    <Routes />
+    <ApolloProvider client={apolloClient}>
+      <Routes />
+    </ApolloProvider>
   </Router>,
   document.getElementById('root')
 );
