98e592f251c71eb475a851d4aeb6a1393170f682
doglasbatista <doglasbatista@gmail.com>
2019-12-11 18:39:40 -0300

    Create input component with floating label

---
+++ src/components/common/input/Input.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import { Container, StyledInput, Label } from './styles';
+
+const Input = ({ type, label, id, value, onFocus, onChange }) => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const inputIsFilled = Boolean(value);
+
+  const handleOnBlur = () => {
+    setIsFocused(false);
+  };
+
+  const handleOnFocus = () => {
+    setIsFocused(true);
+    onFocus();
+  };
+
+  return (
+    <Container>
+      <StyledInput
+        type={type}
+        id={id}
+        onFocus={handleOnFocus}
+        onBlur={handleOnBlur}
+        onChange={onChange}
+      />
+      <Label htmlFor={id} up={inputIsFilled || isFocused}>
+        {label}
+      </Label>
+    </Container>
+  );
+};
+
+Input.propTypes = {
+  type: PropTypes.oneOf(['text']),
+  label: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  onFocus: PropTypes.func,
+  onChange: PropTypes.func,
+};
+
+Input.defaultProps = {
+  type: 'text',
+  value: '',
+  onFocus: () => {},
+  onChange: () => {},
+};
+
+export default Input;
---
+++ src/components/common/input/styles.js
@@ -0,0 +1,33 @@
+import styled, { css } from 'styled-components';
+
+const Container = styled.div`
+  position: relative;
+`;
+
+const StyledInput = styled.input`
+  font-size: 14px;
+  width: 100%;
+  height: 45px;
+  padding: 6px;
+  border: none;
+  border-bottom: 1px solid #bababa;
+  outline: none;
+`;
+
+const Label = styled.label`
+  font-size: 16px;
+  position: absolute;
+  pointer-events: none;
+  left: 6px;
+  bottom: 8px;
+  transition: 0.3s ease all;
+
+  ${({ up }) =>
+    up &&
+    css`
+      bottom: 36px;
+      opacity: 0.6;
+    `}
+`;
+
+export { Container, StyledInput, Label };
