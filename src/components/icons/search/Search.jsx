835c178d64bb0517dcf84649f7b2126457cf0a59
doglasbatista <doglasbatista@gmail.com>
2019-12-11 19:06:51 -0300

    Added search icon

---
+++ src/components/icons/search/Search.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+
+const Search = props => (
+  <svg width="1em" height="1em" viewBox="0 0 19 18" fill="none" {...props}>
+    <path
+      d="M8.993 15.222c3.964 0 7.176-3.183 7.176-7.11C16.17 4.183 12.957 1 8.993 1 5.03 1 1.817 4.184 1.817 8.111c0 3.928 3.213 7.111 7.176 7.111zM17.963 17l-3.902-3.867"
+      stroke="#4F4F4F"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default Search;
