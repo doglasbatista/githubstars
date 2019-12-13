f68b938e926d5b53faee785f01ad3c2f970e893b
doglasbatista <doglasbatista@gmail.com>
2019-12-13 09:20:46 -0300

    Added new icons

---
+++ src/components/icons/mail/Mail.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+
+const Mail = props => (
+  <svg width="1em" height="1em" viewBox="0 0 16 13" fill="none" {...props}>
+    <path
+      d="M2.333 1H13c.733 0 1.333.614 1.333 1.366v8.193c0 .75-.6 1.365-1.333 1.365H2.333C1.6 11.924 1 11.31 1 10.56V2.366C1 1.614 1.6 1 2.333 1z"
+      stroke="#B1B1E6"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M14.333 2.366L7.667 7.145 1 2.365"
+      stroke="#B1B1E6"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default Mail;
