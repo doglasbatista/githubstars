3137f134afb3864192d5e8ba33008421b3cbc784
doglasbatista <doglasbatista@gmail.com>
2019-12-14 15:39:43 -0300

    Added new color

--- src/colors.js
+++ src/colors.js
@@ -1,5 +1,6 @@
 const primaryColor = '#4d4ca2';
 const primaryColorLight = '#5352b2';
+const primaryColorDarken = '#5152B7';
 
 const neutralColor = '#4F4F4F';
 
@@ -7,4 +8,11 @@ const white = '#fff';
 
 const black = '#000';
 
-export { primaryColor, primaryColorLight, neutralColor, white, black };
+export {
+  primaryColor,
+  primaryColorLight,
+  primaryColorDarken,
+  neutralColor,
+  white,
+  black,
+};
